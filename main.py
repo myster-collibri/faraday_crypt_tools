f76887166552dcfe7ce41951e00883f7b5871049
innova <hummingb420@gmail.com>
2022-04-22 15:39:01 +0000

    TroisiemeCommit

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello world")
+""" Ceci est un script de cryptage utilisant la methode AES et le mode CBC
+    créer par Kernel le 22 avril 2022 .
+    le code est libre et gratuit donc aidez moi a l'ameliorer
+"""
